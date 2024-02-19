2817f790f5d92cb4fe1263799d77ed3555ea1808
Onur <>
2024-02-19 13:00:27 +0300

    Project created.

---
+++ DogList/DogList/DogListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogListApp.swift
+//  DogList
+//
+//  Created by Onur on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DogListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
---
+++ DogList/DogList/SplashScreenView.swift
@@ -0,0 +1,42 @@
+//
+//  SplashScreenView.swift
+//  DogList
+//
+//  Created by Onur on 19.02.2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    Image("ic_splash")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.white)
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
